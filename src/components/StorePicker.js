3edb06bceb4728dd517f996e968b89a23999647d
dineshbogolu <visitdinu@gmail.com>
2019-03-04 23:00:41 +0530

    Add refs and events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 export default class StorePicker extends React.Component {
+  txtStoreName = React.createRef();
+  gotoStore = event => {
+    event.preventDefault();
+    const storeName = this.txtStoreName.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.gotoStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store name" />
+        <input
+          type="text"
+          required
+          placeholder="Store name"
+          defaultValue={getFunName()}
+          ref={this.txtStoreName}
+        />
         <button type="submit">Visit Store -></button>
       </form>
     );
